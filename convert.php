9bb1e4313abe8211192f4d3b57acad613bc99185
arnezthe <arnezthe@gmail.com>
2019-06-24 10:48:35 +0700

    convert.php
    Source to get orderbook of any asset pair
    https://matcher.wavesplatform.com/matcher/orderbook/' . $asset2 . '/' . $asset1 . '/status
    
    All orderbook asset records: https://matcher.wavesplatform.com/matcher/orderbook
    
    Source to get transaction of any wallet address
    https://nodes.wavesnodes.com/transactions/address/' . $address . '/limit/' . $limit

---
+++ convert.php
@@ -0,0 +1,38 @@
+```
+<?php
+/** Converting price for matcher [default WAVES: 8 decimal]
+* $n = amount or price
+* $asset1_dec and $asset2_dec = the decimal of any asset
+*/
+function convert_price($n, $asset2_dec, $asset1_dec = 8)
+{
+    $price = ($n / 10 ** (8 - ($asset2_dec - $asset1_dec)));
+
+    return number_format($price, 8, '.', ',');
+}
+
+/** Converting amount for matcher */
+function convert_amount($n, $asset2_dec, $asset1_dec = 8)
+{
+    $amount = ($n / 10 ** (8 + ($asset2_dec - $asset1_dec)));
+
+    return number_format($amount, 8, '.', ',');
+}
+
+/** Rounded amount for transaction */
+function convert_amount_tx($n, $asset2_dec, $asset1_dec = 8)
+{
+    $amount = ($n / 10 ** (8 - ($asset2_dec - $asset1_dec)));
+
+    return $amount;
+    //then number_format($amount, 8, '.', ',');
+}
+
+/** Rounded price for transaction */
+function convert_price_tx($n, $asset2_dec, $asset1_dec = 8)
+{
+    $price = ($n / 10 ** (8 + ($asset2_dec - $asset1_dec)));
+
+    return $price;
+    //then number_format($price, 8, '.', ',');
+}```
